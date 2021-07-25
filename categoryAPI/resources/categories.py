4894b01bb745bdd90d8a27835cbd3700ef43d25d
AyushSoni-7 <ayushsoni47.as@gmail.com>
2021-07-25 13:03:23 +0530

    added flask sqlalchemy

--- categoryAPI/resources/categories.py
+++ categoryAPI/resources/categories.py
@@ -1,22 +1,56 @@
-from flask_restful import Resource
-from flask import request
-from data.data import CategoriesData
+from flask_restful import Resource, reqparse
+from models.category import Category
 
 
 class Categories(Resource):
+    parser = reqparse.RequestParser()
+    parser.add_argument('id',
+                        type=int,
+                        required=False,
+                        store_missing=False,
+                        help="Post request an id."
+                        )
+    parser.add_argument('name',
+                        type=str,
+                        required=True,
+                        help="Name of the category"
+                        )
+    parser.add_argument('description',
+                        type=str,
+                        required=True,
+                        help="Description of the category"
+                        )
+    parser.add_argument('img_src',
+                        type=str,
+                        required=True,
+                        help="Source URL of the category image"
+                        )
 
     def get(self):
-        data = CategoriesData
-        return {'categories': data}, 200
+        return {'categories': Category.get_all()}, 200
 
     def post(self):
-        data = request.get_json()
-        return data, 200
+        data = self.parser.parse_args()
+        category = Category(**data)
+        category.save()
+        try:
+            category.save()
+        except Exception:
+            return {"message": "An error occurred"}, 500
+        return category.json(), 201
 
     def put(self):
-        data = request.get_json()
-        categoryData = next(
-            filter(lambda x: x['id'] == data['id'], CategoriesData)
-            )
-        categoryData.update(data)
-        return categoryData, 200
+        data = self.parser.parse_args()
+        category = Category.get(data['id'])
+        if category:
+            category.name = data['name']
+            category.description = data['description']
+            category.img_src = data['img_src']
+        else:
+            del data['id']
+            category = Category(**data)
+        try:
+            category.save()
+        except Exception:
+            return {"message": "An error occurred"}, 500
+        return category.json(), 201
--- categoryAPI/resources/category.py
+++ categoryAPI/resources/category.py
@@ -1,18 +1,18 @@
 from flask_restful import Resource
-from data.data import CategoriesData
+from models.category import Category as cModel
 
 
 class Category(Resource):
 
     def get(self, category_id: int):
-        categoryData = next(
-            filter(lambda x: x['id'] == category_id, CategoriesData)
-            )
-        return categoryData, 200
+        category = cModel.get(category_id)
+        if category:
+            return category.json(), 200
+        return {'message': 'Category Not Found'}, 404
 
     def delete(self, category_id: int):
-        global CategoriesData
-        CategoriesData = list(
-            filter(lambda x: x['id'] != category_id, CategoriesData)
-            )
-        return {'message': 'Item deleted'}, 200
+        category = cModel.get(category_id)
+        if category:
+            category.delete()
+            return {'message': 'Category Deleted'}
+        return {'message': 'Category Not Found'}, 404
